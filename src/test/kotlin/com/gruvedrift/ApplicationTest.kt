3834b9f5accbcbfe14c28254ff3cf98b9885fa07
gruvedrift <william.gruvedrift@gmail.com>
2023-12-09 11:52:35 +0100

    fixed testing to use ApplicationTestBuilder object

--- src/test/kotlin/com/gruvedrift/ApplicationTest.kt
+++ src/test/kotlin/com/gruvedrift/ApplicationTest.kt
@@ -1,22 +1,30 @@
 package com.gruvedrift
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.application.*
 import io.ktor.server.testing.*
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ApplicationTest {
+    @Test
+    fun getOrderTotal() = testApplication {
+        val response = client.get("/order/1/total")
+        assertEquals(
+            """841.95""",
+            response.bodyAsText()
+        )
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
 
     @Test
-    fun testGetCustomer() = withTestApplication(Application::module) {
-        val response = handleRequest(HttpMethod.Get, "/customer").response
+    fun getCustomer() = testApplication {
+        val response = client.get("/customer")
         assertEquals(
-            """[{"id":"666","firstName":"David","lastName":"Goggins","email":"stay.hard@gmale.com"}]"""
-            , response.content
+            """[{"id":"666","firstName":"David","lastName":"Goggins","email":"stay.hard@gmale.com"}]""" ,
+            response.bodyAsText()
         )
-        assertEquals(
-            HttpStatusCode.OK, response.status()
-        )
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 }
